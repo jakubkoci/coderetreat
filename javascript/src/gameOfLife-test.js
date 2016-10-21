3c7e0d7a4fbf8251c23b7d18fe66460115c1c1a4
jakubkoci <jakub.koci@gmail.com>
2016-10-21 23:29:57 +0200

    Basic project setup

---
+++ javascript/src/gameOfLife-test.js
@@ -0,0 +1,8 @@
+import { expect } from 'chai'
+import * as gameOfLife from './gameOfLife'
+
+describe('setup test', () => {
+  it('when everything is setup correctly I will be green', () => {
+    expect('everything').to.be.ok;
+  })
+})
